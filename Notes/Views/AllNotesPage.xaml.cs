97378d333261f6a1e8089b8bf4a50072a1a0c882
MOEURN-Lykheang <ly.kheang.jr@gmail.com>
2024-09-23 21:18:40 +0700

    Add project files.

---
+++ Notes/Views/AllNotesPage.xaml.cs
@@ -0,0 +1,15 @@
+namespace Notes.Views;
+
+public partial class AllNotesPage : ContentPage
+{
+    public AllNotesPage()
+    {
+        InitializeComponent();
+    }
+
+    private void ContentPage_NavigatedTo(object sender, NavigatedToEventArgs e)
+    {
+        notesCollection.SelectedItem = null;
+    }
+
+}
